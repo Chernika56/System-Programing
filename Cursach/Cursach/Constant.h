ee75aa3895aba16723245efa9661608b0eadf2cf
Chernika_56 <bosko.working@mail.ru>
2023-12-13 01:41:40 +0300

    add Timer and Statistics

--- Cursach/Cursach/Constant.h
+++ Cursach/Cursach/Constant.h
@@ -2,12 +2,5 @@
 
 namespace ProjConst
 {
-	const int WND_INI_WIDTH = 1000;
-	const int WND_INI_HEIGHT = 800;
-
-	const wchar_t* WND_MAIN_CLASS_NAME = L"MainWnd";
-	const wchar_t* WND_MAIN_CAPTION = L"Circles";
-	const int CIRCEL_RADIUS = 25;
-
-	const COLORREF WND_MAIN_INI_COLOR = RGB(255, 255, 255);
+	
 }
--- Cursach/Cursach/Source.cpp
+++ Cursach/Cursach/Source.cpp
@@ -8,13 +8,9 @@
 #include <list>
 #include <chrono>
 
-std::vector<std::thread> circleThreads;
-std::atomic<bool> drawCircles(true);
-std::wstring cursorInfo;
-
 HINSTANCE hInst;
-HWND hwndMain, hwndInput, hwndSpeed_Edit, hwndSpeed_Button, hwndCyrcle, hwndInput_Button;
-int speed;
+HWND hwndMain, hwndInput, hwndSpeed_Edit, hwndNum_Edit,  hwndCyrcle;
+int speed = 0, circleNumMax = 0, circleNum = 0, circleNumDel = 0, clickNum = 0;
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 LRESULT CALLBACK InputWndProc(HWND, UINT, WPARAM, LPARAM);
@@ -25,12 +21,14 @@ class CircleEvent {
 public:
 	int x;
 	int y;
+	bool visible;
 	std::chrono::steady_clock::time_point appearanceTimestamp;
 	std::chrono::steady_clock::time_point removalTimestamp;
 
-	CircleEvent(int _x, int _y, std::chrono::steady_clock::time_point _appearanceTimestamp, 
+	CircleEvent(int _x, int _y, bool _visible, std::chrono::steady_clock::time_point _appearanceTimestamp, 
 		std::chrono::steady_clock::time_point _removalTimestamp)
-		: x(_x), y(_y), appearanceTimestamp(_appearanceTimestamp), removalTimestamp(_removalTimestamp) {}
+		: x(_x), y(_y), visible(_visible), appearanceTimestamp(_appearanceTimestamp), 
+		removalTimestamp(_removalTimestamp) {}
 
 	void setRemovalTimestamp(std::chrono::steady_clock::time_point removalTime) {
 		removalTimestamp = removalTime;
@@ -40,25 +38,41 @@ public:
 // Очередь для хранения событий щелчков
 std::list<CircleEvent> circleEventsList;
 
-void DrawCircles(HWND hwnd) 
-{
-	while (drawCircles) 
-	{
-		Sleep(5000 / speed);
+void DrawCircles(HWND hwnd) {
+	int x = rand() % 800 + 50;
+	int y = rand() % 600 + 50;
 
-		int x = rand() % 800 + 50;
-		int y = rand() % 600 + 50;
+	HDC hdc = GetDC(hwnd);
+	HBRUSH brush = CreateSolidBrush(RGB(rand() % 256, rand() % 256, rand() % 256));
+	SelectObject(hdc, brush);
+	Ellipse(hdc, x - 25, y - 25, x + 25, y + 25);
+	DeleteObject(brush);
+	ReleaseDC(hwnd, hdc);
 
-		HDC hdc = GetDC(hwnd);
-		HBRUSH brush = CreateSolidBrush(RGB(rand() % 256, rand() % 256, rand() % 256));
-		SelectObject(hdc, brush);
-		Ellipse(hdc, x - 25, y - 25, x + 25, y + 25);
-		DeleteObject(brush);
-		ReleaseDC(hwnd, hdc);
+	std::chrono::steady_clock::time_point appearanceTime = std::chrono::steady_clock::now();
+	circleEventsList.push_back(CircleEvent(x, y, true, appearanceTime, appearanceTime));
+}
 
-		std::chrono::steady_clock::time_point appearanceTime = std::chrono::steady_clock::now();
-		circleEventsList.push_back(CircleEvent(x, y, appearanceTime, appearanceTime));
+void dataOutput(HWND hwnd) {
+	int interval = 0;
+
+	for (auto it = circleEventsList.begin(); it != circleEventsList.end(); it++) {
+		auto time1 = it->appearanceTimestamp.time_since_epoch();
+		int milliseconds1 = static_cast<int>(std::chrono::duration_cast<std::chrono::milliseconds>(time1).count());
+
+		auto time2 = it->removalTimestamp.time_since_epoch();
+		int milliseconds2 = static_cast<int>(std::chrono::duration_cast<std::chrono::milliseconds>(time2).count());
+
+		interval += milliseconds2 - milliseconds1;
 	}
+
+	int stat1 = interval / circleEventsList.size();
+
+	std::wstring massageText = L"Среднее время реакции:\n" + 
+		std::to_wstring(stat1) + L" миллисекунд\n\nТочность:\n" + 
+		std::to_wstring((float)circleEventsList.size() / (clickNum) * 100) + L"%";
+
+	MessageBox(hwnd, massageText.c_str(), _T("Результат"), MB_OK);
 }
 
 void CreateMainWindow() {
@@ -110,9 +124,9 @@ void CreateInputWindow() {
 	RegisterClassEx(&wcex);
 
     hwndInput = CreateWindow(
-        _T("InputWindowClass"), _T("Окно ввода скорости"),
+        _T("InputWindowClass"), _T("Окно ввода"),
         WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 400, 200,
+        CW_USEDEFAULT, CW_USEDEFAULT, 300, 250,
         NULL, NULL, hInst, NULL
     );
 
@@ -148,6 +162,8 @@ void CreateCircleWindow() {
 
     ShowWindow(hwndCyrcle, SW_SHOWNORMAL);
     UpdateWindow(hwndCyrcle);
+
+	SetTimer(hwndCyrcle, 1, 5000 / speed, NULL);
 }
 
 // Основная функция WinAPI
@@ -166,14 +182,14 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 }
 
 // Обработчик сообщений для главного окна
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
     switch (message) {
     case WM_CREATE:
         CreateWindow(
             _T("BUTTON"), _T("Открыть окно ввода скорости"),
             WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
             100, 50, 200, 50,
-            hWnd, (HMENU)1, hInst, NULL
+            hwnd, (HMENU)1, hInst, NULL
         );
 
         break;
@@ -188,96 +204,132 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 
 		break;
     default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
+        return DefWindowProc(hwnd, message, wParam, lParam);
     }
 
     return 0;
 }
 
 // Обработчик сообщений для окна ввода скорости
-LRESULT CALLBACK InputWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+LRESULT CALLBACK InputWndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
     switch (message) {
     case WM_CREATE:
+		CreateWindow(
+			_T("STATIC"), _T("Введите скорость"),
+			WS_CHILD | WS_VISIBLE,
+			20, 10, 150, 30,
+			hwnd, NULL, hInst, NULL
+		);
+
         hwndSpeed_Edit = CreateWindow(
             _T("EDIT"), _T(""),
             WS_CHILD | WS_VISIBLE | WS_BORDER | ES_NUMBER,
-            10, 10, 150, 30,
-            hWnd, (HMENU)3, hInst, NULL
+            20, 40, 150, 30,
+            hwnd, NULL, hInst, NULL
         );
+
+		CreateWindow(
+			_T("STATIC"), _T("Введите число кругов"),
+			WS_CHILD | WS_VISIBLE,
+			20, 80, 150, 30,
+			hwnd, NULL, hInst, NULL
+		);
+
+		hwndNum_Edit = CreateWindow(
+			_T("EDIT"), _T(""),
+			WS_CHILD | WS_VISIBLE | WS_BORDER | ES_NUMBER,
+			20, 110, 150, 30,
+			hwnd, NULL, hInst, NULL
+		);
 
         CreateWindow(
             _T("BUTTON"), _T("Вывести сообщение"),
             WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-            10, 50, 150, 30,
-            hWnd, (HMENU)4, hInst, NULL
+            20, 150, 150, 30,
+            hwnd, (HMENU)4, hInst, NULL
         );
 
         break;
     case WM_COMMAND:
         if (LOWORD(wParam) == 4) {
             TCHAR buffer[256];
-            GetWindowText(hwndSpeed_Edit, buffer, sizeof(buffer) / sizeof(buffer[0]));
 
+            GetWindowText(hwndSpeed_Edit, buffer, sizeof(buffer) / sizeof(buffer[0]));
             speed = _tstoi(buffer);
+
+			GetWindowText(hwndNum_Edit, buffer, sizeof(buffer) / sizeof(buffer[0]));
+			circleNumMax = _tstoi(buffer);
 
             CreateCircleWindow();
 
-            DestroyWindow(hWnd);
+            DestroyWindow(hwnd);
         }
 
         break;
     case WM_CLOSE:
-        DestroyWindow(hWnd);
+        DestroyWindow(hwnd);
 
         break;
     default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
+        return DefWindowProc(hwnd, message, wParam, lParam);
     }
 
     return 0;
 }
 
-LRESULT CALLBACK CircleWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+LRESULT CALLBACK CircleWndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
 	switch (message) {
 	case WM_CREATE:
+		circleNum = 0;
+		circleNumDel = 0;
+		clickNum = 0;
 		circleEventsList.clear();
-		srand(time(nullptr));
-		std::thread(DrawCircles, hWnd).detach();
+
+		break;
+	case WM_TIMER:
+		if (circleNum < circleNumMax) {
+			DrawCircles(hwnd);
+			circleNum++;
+		}
+		else {
+			if (circleNumDel == circleNumMax) {
+				DestroyWindow(hwnd);
+			}
+		}
 
 		break;
 	case WM_SIZE:
-		InvalidateRect(hWnd, NULL, true);
+		InvalidateRect(hwnd, NULL, true);
 
 		break;
 	case WM_CHAR:
 	{
-		if (wParam == 'W' || wParam == 'w')
-		{
+		if (wParam == 'W' || wParam == 'w') {
 			std::wstringstream ss;
 			POINT cursorPos;
 			GetCursorPos(&cursorPos);
-			ScreenToClient(hWnd, &cursorPos);
+			ScreenToClient(hwnd, &cursorPos);
 
 			int mouseX = cursorPos.x;
 			int mouseY = cursorPos.y;
 
-			for (auto it = circleEventsList.begin(); it != circleEventsList.end(); it++)
-			{
+			for (auto it = circleEventsList.begin(); it != circleEventsList.end(); it++) {
 				int x = it->x;
 				int y = it->y;
 
-				if ((mouseX - x) * (mouseX - x) + (mouseY - y) * (mouseY - y) <= 25 * 25)
-				{
+				if ((mouseX - x) * (mouseX - x) + (mouseY - y) * (mouseY - y) <= 25 * 25) {
 					std::chrono::steady_clock::time_point removalTime = std::chrono::steady_clock::now();
 					it->setRemovalTimestamp(removalTime);
+					it->visible = false;
 
-					circleEventsList.erase(it);
+					circleNumDel++;
 
-					InvalidateRect(hWnd, NULL, TRUE);
+					InvalidateRect(hwnd, NULL, TRUE);
 					break;
 				}
-			}	
-		
+			}
+
+			clickNum++;
 		}
 	}
 	break;
@@ -285,58 +337,52 @@ LRESULT CALLBACK CircleWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lP
 	{
 		int mouseX = LOWORD(lParam);
 		int mouseY = HIWORD(lParam);
-		for (auto it = circleEventsList.begin(); it != circleEventsList.end(); it++)
-		{
+		for (auto it = circleEventsList.begin(); it != circleEventsList.end(); it++) {
 			int x = it->x;
 			int y = it->y;
 
-			if ((mouseX - x) * (mouseX - x) + (mouseY - y) * (mouseY - y) <= 25 * 25)
-			{
+			if ((mouseX - x) * (mouseX - x) + (mouseY - y) * (mouseY - y) <= 25 * 25) {
 				std::chrono::steady_clock::time_point removalTime = std::chrono::steady_clock::now();
 				it->setRemovalTimestamp(removalTime);
+				it->visible = false;
 
-				circleEventsList.erase(it);
+				circleNumDel++;
 
-				InvalidateRect(hWnd, NULL, TRUE);
+				InvalidateRect(hwnd, NULL, TRUE);
 				break;
 			}
 		}
+
+		clickNum++;
 	}
 	break;
-	case WM_RBUTTONDOWN:
-		drawCircles = false;
-
-		break;
 	case WM_PAINT:
 	{
 		PAINTSTRUCT ps;
-		HDC hdc = BeginPaint(hWnd, &ps);
+		HDC hdc = BeginPaint(hwnd, &ps);
 
-		for (auto it = circleEventsList.begin(); it != circleEventsList.end(); it++)
-		{
-			int x = it->x;
-			int y = it->y;
+		for (auto it = circleEventsList.begin(); it != circleEventsList.end(); it++) {
+			if (it->visible) {
+				int x = it->x;
+				int y = it->y;
 
-			HBRUSH brush = CreateSolidBrush(RGB(rand() % 256, rand() % 256, rand() % 256));
-			SelectObject(hdc, brush);
-			Ellipse(hdc, x - 25, y - 25, x + 25, y + 25);
-			DeleteObject(brush);
+				HBRUSH brush = CreateSolidBrush(RGB(rand() % 256, rand() % 256, rand() % 256));
+				SelectObject(hdc, brush);
+				Ellipse(hdc, x - 25, y - 25, x + 25, y + 25);
+				DeleteObject(brush);
+			}
 		}
 
-		EndPaint(hWnd, &ps);
+		EndPaint(hwnd, &ps);
 	}
 	break;
-	case WM_CLOSE:
-		for (std::thread& thread : circleThreads)
-		{
-			thread.join();
-		}
-		//circleEventsList.clear();
-		DestroyWindow(hWnd);
+	case WM_DESTROY:
+		KillTimer(hwndCyrcle, 1);
+		dataOutput(hwnd);
 
 		break;
 	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
+		return DefWindowProc(hwnd, message, wParam, lParam);
 	}
 	return 0;
 }
